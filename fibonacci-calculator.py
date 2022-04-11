884d8783ba20f41e0fb8bc64e56f45c1fd92818e
Gagan Kaul <gagankaul@gmail.com>
2022-04-11 15:03:46 -0400

    update

--- fibonacci-calculator.py
+++ fibonacci-calculator.py
@@ -5,7 +5,7 @@ print("#Welcome to the Fibonacci Calculator App\n")
 #Getting user input
 num_digits = int(input("how many digits of the Fibonacci sequence would you like to compute? "))
 
-#Calculating the printing the Fibonacci sequence
+#Compute and display the Fibonacci sequence
 print(f"\nThe first {num_digits} numbers of the Fibonacci sequence are:")
 
 num1 = 1
@@ -19,7 +19,8 @@ for i in range(num_digits):
   num1 = num2
   num2 = num
 
-print(f"\nThe golden ratios are: ")
+#Compute and display golden ratio values
+print(f"\nThe corresponding golden ratios are: ")
 for i in range(1,len(series)):
   ratio = series[i] / series[i-1]
   print(ratio)
